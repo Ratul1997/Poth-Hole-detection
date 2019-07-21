6d156bcfd8c9c20ac7589ed6a59fb4b52bd73e9c
Ratul1997 <ratulbhowmick66@gmail.com>
2019-07-21 20:00:34 +0600

    as

--- Video_img.py
+++ Video_img.py
@@ -23,42 +23,13 @@ cv2.createTrackbar("TH", "Trackbar", 0 , 255, nothing)
 def cropImage(frame):
     height, width, _ = frame.shape
     points = np.array([
-        [(0, height), (135, 260), (290, 260), (width, 450), (width, height)]])
+        [(30, height), (132, 260), (290, 260),(width, 450),(width,height) ]])
 
     mask = np.zeros((height, width), np.uint8)
     cv2.polylines(mask, np.int32([points]), True, 255, 2)
     cv2.fillPoly(mask, np.int32([points]), 255)
     masked_image = cv2.bitwise_and(frame, frame, mask=mask)
     return masked_image
-
-
-def histrigramEqu(img):
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    equ = cv2.equalizeHist(img)
-    # res = np.hstack((img,equ))
-
-    blur = cv2.GaussianBlur(equ, (5, 5), 0)
-
-    cv2.imshow("ss", equ)
-    cv2.imshow("bl", blur)
-
-    TH = cv2.getTrackbarPos("TH", "Trackbar")
-
-    _, res2 = cv2.threshold(blur, TH, 255, cv2.THRESH_BINARY_INV)
-
-    cv2.imshow("th", res2)
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-    erosion = cv2.erode(res2, kernel, iterations=2)
-
-    res2 = cv2.dilate(erosion, kernel, iterations=4)
-
-    cv2.imshow("th2", res2)
-    res2 = cv2.morphologyEx(res2, cv2.MORPH_OPEN, kernel)
-
-    cv2.imshow("th3", res2)
-    print(np.array(res2))
-
 
 def cnvrtHSV(frame):
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -80,10 +51,7 @@ def cnvrtHSV(frame):
 
 def histrigramEqu(img):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # print(np.array(img))
     equ = cv2.equalizeHist(img)
-    # res = np.hstack((img,equ))
-
     blur = cv2.GaussianBlur(equ, (5, 5), 0)
 
     cv2.imshow("ss", equ)
@@ -91,7 +59,7 @@ def histrigramEqu(img):
 
     TH = cv2.getTrackbarPos("TH", "Trackbar")
 
-    _, res2 = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY)
+    _, res2 = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV)
 
     cv2.imshow("th", res2)
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
@@ -106,21 +74,20 @@ def histrigramEqu(img):
 
     return res2
 
-
-def contrs(img,cpy):
+def contrs(img,frame):
     mask = np.zeros(img.shape, np.uint8)
     contours, _ = cv2.findContours(img, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     for cnt in contours:
-        print(cv2.contourArea(cnt))
-        if 200 < cv2.contourArea(cnt) < 110500:
+        # print(cv2.contourArea(cnt))
+        if 420 < cv2.contourArea(cnt) < 120380:
             print("sss " + str(cv2.contourArea(cnt)))
-            cv2.drawContours(cpy, [cnt], 0, (0, 255, 0), 2)
-            cv2.drawContours(mask, [cnt], 0, 255, -1)
+            # cv2.drawContours(frame, [cnt], 0, (0, 255, 0), thickness=cv2.FILLED)
+            x, y, w, h = cv2.boundingRect(cnt)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-    cv2.imshow("th4", mask)
-    cv2.bitwise_not(img, img, mask)
-    cv2.imshow('IMG', cpy)
-    return img
+
+    cv2.imshow("th4", frame)
+    return mask
 
 cap = cv2.VideoCapture('video/1.mp4')
 
--- pth.py
+++ pth.py
@@ -24,42 +24,13 @@ cv2.createTrackbar("TH", "Trackbar", 0 , 255, nothing)
 def cropImage(frame):
     height, width, _ = frame.shape
     points = np.array([
-        [(0, height), (127, 260), (290, 260),(width, 450),(width,height) ]])
+        [(15, height), (127, 260), (290, 260),(width, 450),(width,height) ]])
 
     mask = np.zeros((height, width), np.uint8)
     cv2.polylines(mask, np.int32([points]), True, 255, 2)
     cv2.fillPoly(mask, np.int32([points]), 255)
     masked_image = cv2.bitwise_and(frame, frame, mask=mask)
     return masked_image
-
-
-def histrigramEqu(img):
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    equ = cv2.equalizeHist(img)
-    # res = np.hstack((img,equ))
-
-    blur = cv2.GaussianBlur(equ, (5, 5), 0)
-
-    cv2.imshow("ss", equ)
-    cv2.imshow("bl", blur)
-
-    TH = cv2.getTrackbarPos("TH", "Trackbar")
-
-    _, res2 = cv2.threshold(blur, TH, 255, cv2.THRESH_BINARY_INV)
-
-    cv2.imshow("th", res2)
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-    erosion = cv2.erode(res2, kernel, iterations=2)
-
-    res2 = cv2.dilate(erosion, kernel, iterations=4)
-
-    cv2.imshow("th2", res2)
-    res2 = cv2.morphologyEx(res2, cv2.MORPH_OPEN, kernel)
-
-    cv2.imshow("th3", res2)
-    print(np.array(res2))
-
 
 def cnvrtHSV(frame):
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -107,19 +78,18 @@ def histrigramEqu(img):
 
     return res2
 
-def contrs(img):
+def contrs(img,frame):
     mask = np.zeros(img.shape, np.uint8)
     contours, _ = cv2.findContours(img, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     for cnt in contours:
         print(cv2.contourArea(cnt))
-        if 200 < cv2.contourArea(cnt) < 180380:
+        if 200 < cv2.contourArea(cnt) < 120380:
             print("sss " + str(cv2.contourArea(cnt)))
-            cv2.drawContours(img, [cnt], 0, (0, 255, 0), thickness=cv2.FILLED)
-            cv2.drawContours(mask, [cnt], 0, 255, -1)
+            cv2.drawContours(frame, [cnt], 0, (0, 255, 0), thickness=cv2.FILLED)
 
 
 
-    cv2.imshow("th4", mask)
+    cv2.imshow("th4", frame)
     return mask
 
 while True:
@@ -136,7 +106,7 @@ while True:
     cv2.imshow("frame2",frame)
 
     frame = histrigramEqu(frame)
-    mask = contrs(frame)
+    mask = contrs(frame,cpy_frame)
 
     red = cv2.bitwise_and(cpy_frame, cpy_frame, mask=mask)
 
